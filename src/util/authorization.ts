845c3cd85d54837d9c43020faebae38d4ad59e2d
king-aggor <109685452+king-aggor@users.noreply.github.com>
2024-11-01 16:09:44 +0000

    customer authorization

--- src/util/authorization.ts
+++ src/util/authorization.ts
@@ -14,3 +14,20 @@ export const admin = async (authToken: string) => {
     throw new CustomError(err.message, err.statusCode);
   }
 };
+
+//verify customer
+export const customer = async (authToken: string) => {
+  try {
+    const tokenData: { id: string; user: string } = await jwt.verifyToken(
+      authToken
+    );
+    //check if user is admin
+    const user = tokenData.user.toLocaleLowerCase();
+    if (user !== "customer") {
+      throw new CustomError("unauthorized user: user must be a customer", 493);
+    }
+    return tokenData;
+  } catch (err: any) {
+    throw new CustomError(err.message, err.statusCode);
+  }
+};
